be83bdf3e23f17089738e8a40b545e9a6ad54011
sskiragu <skiragu@gmail.com>
2023-07-05 18:36:43 +0300

    /signup & /login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class AuthController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function signup(Request $request){
+        $user = new User();
+
+        $user->username =  $request->username;
+        $user->email =  $request->email;
+        $user->password =  $request->password;
+
+        $user->save();
+    }
+
+    public function login(Request $request){
+        $credentials = $request->only(['username', 'password']);
+        
+        if (Auth::attempt($credentials)) {
+            return response()->json(['message' => "Login successful."], 200);
+        }else{
+            return response()->json(['message' => "Invalid credentials."], 401);
+        }
+    }
+}
